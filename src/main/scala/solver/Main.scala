bb487fa90e00ef732bab4441e8a718311d768a80
Zach Albia <zach.albia@gmail.com>
2019-08-20 08:56:30 +0800

    feat: add running time to main

--- src/main/scala/solver/Main.scala
+++ src/main/scala/solver/Main.scala
@@ -3,6 +3,11 @@ package solver
 object Main {
   import Piece._
 
-  def main(args: Array[String]): Unit =
-    println(Solver.bestMoveSeq(Game.starting(Hori5, Vert4, RLngL))(4))
+  def main(args: Array[String]): Unit = {
+    val t0 = System.nanoTime()
+    val result = Solver.bestMoveSeq(Game.starting(Hori5, Vert4, One))(12)
+    val t1 = System.nanoTime()
+    println(result)
+    println(s"Elapsed time: ${(t1 - t0) / Math.pow(10, 9)} seconds")
+  }
 }
